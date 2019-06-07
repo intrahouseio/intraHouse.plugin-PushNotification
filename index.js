9dd838cf21956c39e609c14bf8c9a86447f5cd26
System Administrator <fast0490f@mail.ru>
2019-06-07 15:51:40 +0300

    init

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const Plugin = require('./lib/plugin');
+const Notification = require('./lib/notification');
 
 const plugin = new Plugin();
+const notification = new Notification();
 
+
+
+plugin.on('info', data => {
+  data.sendTo.forEach(user => {
+
+  });
+});
 
 plugin.on('start', () => {
-
+  notification.send();
 });
