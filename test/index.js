74dc6637ed2895cba7735d065638dfbae4b61f22
System Administrator <fast0490f@mail.ru>
2019-06-07 15:45:20 +0300

    init

---
+++ test/index.js
@@ -0,0 +1,54 @@
+const child = require('child_process');
+const modulepath = './index.js';
+
+const unitid = 'p2p'
+
+const params = {
+  token: '1888de8d-4c6f-4711-aca9-5b421c210431',
+}
+
+const system = {
+  port: 8088,
+}
+
+const config = [];
+
+const ps = child.fork(modulepath, [unitid]);
+
+ps.on('message', data => {
+  if (data.type === 'get' && data.tablename === `system/${unitid}`) {
+    ps.send({ type: 'get', system });
+  }
+
+  if (data.type === 'get' && data.tablename === `params/${unitid}`) {
+    ps.send({ type: 'get', params });
+  }
+
+  if (data.type === 'get' && data.tablename === `config/${unitid}`) {
+    ps.send({ type: 'get', config: {} });
+  }
+
+  if (data.type === 'data') {
+    console.log('-------------data-------------', new Date().toLocaleString());
+    console.log(data.data);
+    console.log('');
+  }
+
+  if (data.type === 'channels') {
+    console.log('-----------channels-----------', new Date().toLocaleString());
+    console.log(data.data);
+    console.log('');
+  }
+
+  if (data.type === 'debug') {
+    console.log('-------------debug------------', new Date().toLocaleString());
+    console.log(data.txt);
+    console.log('');
+  }
+});
+
+ps.on('close', code => {
+  console.log('close');
+});
+
+ps.send({type: 'debug', mode: true });
